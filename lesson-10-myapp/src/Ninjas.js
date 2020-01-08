8a918e3919cc0aa056291da91f9acb61b9f6cc1d
Polun <polunwu@gmail.com>
2020-01-09 01:17:41 +0800

    adding conditional output

--- lesson-10-myapp/src/Ninjas.js
+++ lesson-10-myapp/src/Ninjas.js
@@ -3,15 +3,19 @@ import React from 'react';
 const Ninjas = ({ ninjas }) => {
 
   const ninjaList = ninjas.map(ninja => {
-    return (
-      <div className="Ninja" key={ninja.id}>
-        <div>Name: {ninja.name}</div>
-        <div>Age: {ninja.age}</div>
-        <div>Belt: {ninja.belt}</div>
-      </div>
-    );
+    if (ninja.age > 20) {
+      return (
+        <div className="Ninja" key={ninja.id}>
+          <div>Name: {ninja.name}</div>
+          <div>Age: {ninja.age}</div>
+          <div>Belt: {ninja.belt}</div>
+        </div>
+      );
+    } else {
+      return null;
+    }
   });
-  console.log(ninjaList);
+
   return (
     <div className="ninja-list">
       {ninjaList}
